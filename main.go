33685fff39f992ddbfc0571b98257417827904e9
David Ko <david.ko@velvetreactor.com>
2018-06-15 15:27:28 -0400

    Attempt at writing a test for TCP server

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"os"
 )
 
@@ -11,6 +14,16 @@ func main() {
 func realMain() int {
 	port := os.Getenv("PORT")
 	if port == "" {
+		return 1
+	}
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", port))
+	if err != nil {
+		log.Fatal(err)
+		return 1
+	}
+	_, err = listener.Accept()
+	if err != nil {
+		log.Fatal(err)
 		return 1
 	}
 	return 0
--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
 	"testing"
 )
 
@@ -13,4 +16,19 @@ func TestMain_requiresPort(t *testing.T) {
 		msg := fmt.Sprintf("Expected exit code %d, got %d", expected, got)
 		t.Error(msg)
 	}
+}
+
+func TestMain_serverWritesFoobar(t *testing.T) {
+	os.Setenv("PORT", "3000")
+	go func() {
+		for {
+			conn, err := net.Dial("tcp", "localhost:3000")
+			if err != nil {
+				log.Fatal(err)
+				continue
+			}
+			fmt.Println(conn)
+		}
+	}()
+	realMain()
 }
